cafce58a303a345d2beb175d4dfb909d782fc085
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-12-01 18:25:34 +0000

    Created an express app and tried a basic mysql query

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-
 var db = require('mysql');
-var connection = db.createConnection({
-    user: 'ziad_saab',
-    host: '127.0.0.1',
-    database: 'addressbook'
-});
-
 var app = express();
 app.use(bodyParser.json());
 
-// app.get('...
+var connection = db.createConnection({
+    user: 'codus',
+    host: 'localhost',
+    database: 'addressbook'
+});
+
+connection.connect();
+
+connection.query("SELECT * FROM Account", function(err, rows){
+    console.log(rows);
+})
+
+connection.end();
