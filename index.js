0b54d2b25846716a4b592e622d6393586d5518a1
Codrin Diaconu <diaconu.codrin@gmail.com>
2015-12-01 19:56:12 +0000

    Exercise #: 3

--- index.js
+++ index.js
@@ -10,20 +10,42 @@ var connection = db.createConnection({
     database: 'addressbook'
 });
 
-
-app.get('/', function(req, res){
-    console.log('The root folder was accessed!')
-    res.send('This is the root folder!');
-})
-
 connection.connect();
 
-connection.query("SELECT * FROM Account", function(err, rows){
-    
+
+app.use('/signIn', function(req, res, next) {
+    req.accountId = 1;
+    next();
 })
 
-connection.end();
+app.get('/signIn/AddressBook', function(req, res) {
+    console.log(req.accountId);
+    connection.query("SELECT * FROM AddressBook where AddressBook.accountId=" + req.accountId, function(err, rows) {
+        res.json(rows);
+    })
+})
 
-app.listen(process.env.PORT, function(){
+
+app.get('/signIn/AddressBook/:id', function(req, res) {
+    connection.query("SELECT * FROM AddressBook where AddressBook.id=" + Number(req.params.id), function(err, rows) {
+        if (rows) {
+            rows.forEach(function(addressbook) {
+                if (addressbook.accountId !== req.accountId) {
+                    res.status(404).send("No matches found!");
+                }
+                else {
+                    res.json(rows);
+                }
+            })
+        } else {
+            res.status(400).send("No matches found!");
+        }
+    })
+
+})
+
+
+
+app.listen(process.env.PORT, function() {
     console.log("Server initialized. Listening on port " + process.env.PORT + '.');
 })
